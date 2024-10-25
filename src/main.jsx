786765d962ef0cd66b36cf49d272c397845115dd
DanSport <bogdan.onoyko@gmail.com>
2024-10-25 16:24:02 +0200

    persistReducer deleted

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,11 @@
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./redux/store";
 import App from "./App";
+import store from "./redux/store";
 
-const container = document.getElementById("root");
-
-if (container) {
-  const root = createRoot(container);
-
-  root.render(
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
